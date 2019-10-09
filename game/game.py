c26f083ac3bb89d38c0f2aabc1f905850b8a747f
alex8223 <alex.emelianov@gmail.com>
2019-10-08 18:34:27 -0700

    drawing real tiles

--- game/game.py
+++ game/game.py
@@ -1,20 +1,26 @@
 import pygame
+import random
 
 pygame.init()
 
 win = pygame.display.set_mode((1600, 1200))
 pygame.display.set_caption("Rally")
+clock = pygame.time.Clock()
 
 class GameBoard(object):
-    def __init__(self):
-        self.rect = (700, 0, 900, 1200)
-        self.floor = pygame.image.load('../assets/floor.png')
+    def __init__(self, board):
+        self.rect = pygame.Rect(700, 0, 900, 1200)
+        self.base = pygame.Surface((self.rect.width, self.rect.height))
+        self.tiles = pygame.image.load("../assets/tiles.png")
+        self.tiles = pygame.transform.smoothscale(self.tiles, (7*75, 13*75))
+        for x in range(12):
+            for y in range(16):
+                self.base.blit(self.tiles, ((75*x, 75*y), (75*x+75, 75*y+75)), area=(75*random.randint(0,6), 75*random.randint(0,12), 75, 75))
+        self.canvas = win.subsurface(self.rect)
+        self.floor = pygame.image.load("../assets/floor.png")
 
     def Draw(self):
-        pygame.draw.rect(win, (20,20,20), self.rect)
-        for x in range (self.rect[0], self.rect[0] + self.rect[2], 75):
-            for y in range(self.rect[1], self.rect[1] + self.rect[3], 75):
-                win.blit(self.floor, (x+1, y+1))
+        self.canvas.blit(self.base, (0,0))
 
 
 class Game(object):
@@ -24,7 +30,6 @@ class Game(object):
 
     def Loop(self):
         while not self.stop:
-            pygame.time.delay(100)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.stop = True
@@ -33,6 +38,7 @@ class Game(object):
                 board.Draw()
 
             pygame.display.update()
+            clock.tick(60)
 
 
 #    keys = pygame.key.get_pressed()
@@ -40,7 +46,7 @@ class Game(object):
 #        x -= vel
 
 game = Game()
-game.boards.append(GameBoard())
+game.boards.append(GameBoard(None))
 game.Loop()
 
 pygame.quit()
