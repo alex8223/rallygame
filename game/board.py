54718859ee6c930103e17901eae9c6f815caad3d
alex8223 <alex.emelianov@gmail.com>
2019-10-15 16:52:45 -0700

    Fixed tile rotations

--- game/board.py
+++ game/board.py
@@ -1,6 +1,15 @@
 class Tile(object):
-    def __init__(self):
+    def __init__(self, speed=None, direction=None):
         self.image = (0, 4)
+        if speed is None:
+            self.speed = 0
+        else:
+            self.speed = speed
+        if direction is None:
+            self.direction = 0
+        else:
+            self.direction = direction
+        self.rotation = (0, 0, 0, 0)
 
 
 def Floor():
@@ -8,7 +17,7 @@ def Floor():
 
 
 def Belt(direction: int, rotation: int) -> Tile:
-    tile = Tile()
+    tile = Tile(speed=1, direction=direction)
     if rotation == 0:
         if direction == 0:
             tile.image = (6, 0)
@@ -20,26 +29,35 @@ def Belt(direction: int, rotation: int) -> Tile:
             tile.image = (6, 2)
     if rotation == 1:
         if direction == 0:
-            tile.image = (4, 2)
-        elif direction == 1:
-            tile.image = (4, 3)
-        elif direction == 2:
-            tile.image = (5, 3)
-        elif direction == 3:
             tile.image = (5, 2)
+            tile.rotation = (0, 0, 0, 1)
+        elif direction == 1:
+            tile.image = (4, 2)
+            tile.rotation = (1, 0, 0, 0)
+        elif direction == 2:
+            tile.image = (4, 3)
+            tile.rotation = (0, 1, 0, 0)
+        elif direction == 3:
+            tile.image = (5, 3)
+            tile.rotation = (0, 0, 1, 0)
     if rotation == -1:
         if direction == 0:
-            tile.image = (4, 1)
-        elif direction == 1:
             tile.image = (5, 1)
-        elif direction == 2:
+            tile.rotation = (0, -1, 0, 0)
+        elif direction == 1:
             tile.image = (5, 0)
-        elif direction == 3:
+            tile.rotation = (0, 0, -1, 0)
+        elif direction == 2:
             tile.image = (4, 0)
+            tile.rotation = (0, 0, 0, -1)
+        elif direction == 3:
+            tile.image = (4, 1)
+            tile.rotation = (-1, 0, 0, 0)
     return tile
 
+
 def FastBelt(direction: int, rotation: int) -> Tile:
-    tile = Tile()
+    tile = Tile(speed=2, direction=direction)
     if rotation == 0:
         if direction == 0:
             tile.image = (1, 4)
@@ -51,92 +69,130 @@ def FastBelt(direction: int, rotation: int) -> Tile:
             tile.image = (2, 5)
     if rotation == 1:
         if direction == 0:
-            tile.image = (2, 2)
-        elif direction == 1:
-            tile.image = (2, 3)
-        elif direction == 2:
-            tile.image = (3, 3)
-        elif direction == 3:
             tile.image = (3, 2)
+            tile.rotation = (0, 0, 0, 1)
+        elif direction == 1:
+            tile.image = (2, 2)
+            tile.rotation = (1, 0, 0, 0)
+        elif direction == 2:
+            tile.image = (2, 3)
+            tile.rotation = (0, 1, 0, 0)
+        elif direction == 3:
+            tile.image = (3, 3)
+            tile.rotation = (0, 0, 1, 0)
     if rotation == -1:
         if direction == 0:
-            tile.image = (2, 1)
-        elif direction == 1:
             tile.image = (3, 1)
-        elif direction == 2:
+            tile.rotation = (0, -1, 0, 0)
+        elif direction == 1:
             tile.image = (3, 0)
-        elif direction == 3:
+            tile.rotation = (0, 0, -1, 0)
+        elif direction == 2:
             tile.image = (2, 0)
+            tile.rotation = (0, 0, 0, -1)
+        elif direction == 3:
+            tile.image = (2, 1)
+            tile.rotation = (-1, 0, 0, 0)
     return tile
+
 
 def MergeBelt(direction: int, cuff: int) -> Tile:
-    tile = Tile()
+    tile = Tile(speed=1, direction=direction)
     if direction == 0 and cuff == 1:
         tile.image = (8, 0)
+        tile.rotation = (0, 0, 0, 1)
     if direction == 0 and cuff == 0:
         tile.image = (8, 4)
+        tile.rotation = (0, -1, 0, 1)
     if direction == 0 and cuff == -1:
         tile.image = (7, 0)
+        tile.rotation = (-1, 0, 0, 0)
     if direction == 1 and cuff == 1:
         tile.image = (8, 1)
+        tile.rotation = (1, 0, 0, 0)
     if direction == 1 and cuff == 0:
         tile.image = (7, 4)
+        tile.rotation = (1, 0, -1, 0)
     if direction == 1 and cuff == -1:
         tile.image = (7, 1)
+        tile.rotation = (0, 0, -1, 0)
     if direction == 2 and cuff == 1:
         tile.image = (8, 2)
+        tile.rotation = (0, 1, 0, 0)
     if direction == 2 and cuff == 0:
         tile.image = (7, 5)
+        tile.rotation = (0, 1, 0, -1)
     if direction == 2 and cuff == -1:
         tile.image = (7, 2)
+        tile.rotation = (0, 0, 0, -1)
     if direction == 3 and cuff == 1:
         tile.image = (8, 3)
+        tile.rotation = (0, 0, 1, 0)
     if direction == 3 and cuff == 0:
         tile.image = (8, 5)
+        tile.rotation = (-1, 0, 1, 0)
     if direction == 3 and cuff == -1:
         tile.image = (7, 3)
+        tile.rotation = (-1, 0, 0, 0)
     return tile
 
+
 def MergeFastBelt(direction: int, cuff: int) -> Tile:
-    tile = Tile()
+    tile = Tile(speed=2, direction=direction)
     if direction == 0 and cuff == 1:
         tile.image = (9, 4)
+        tile.rotation = (0, 0, 0, 1)
     if direction == 0 and cuff == 0:
         tile.image = (10, 3)
+        tile.rotation = (0, -1, 0, 1)
     if direction == 0 and cuff == -1:
         tile.image = (9, 0)
+        tile.rotation = (-1, 0, 0, 0)
     if direction == 1 and cuff == 1:
         tile.image = (9, 5)
+        tile.rotation = (1, 0, 0, 0)
     if direction == 1 and cuff == 0:
         tile.image = (10, 0)
+        tile.rotation = (1, 0, -1, 0)
     if direction == 1 and cuff == -1:
         tile.image = (9, 1)
+        tile.rotation = (0, 0, -1, 0)
     if direction == 2 and cuff == 1:
         tile.image = (10, 5)
+        tile.rotation = (0, 1, 0, 0)
     if direction == 2 and cuff == 0:
         tile.image = (10, 1)
+        tile.rotation = (0, 1, 0, -1)
     if direction == 2 and cuff == -1:
         tile.image = (9, 2)
+        tile.rotation = (0, 0, 0, -1)
     if direction == 3 and cuff == 1:
         tile.image = (10, 4)
+        tile.rotation = (0, 0, 1, 0)
     if direction == 3 and cuff == 0:
         tile.image = (10, 2)
+        tile.rotation = (-1, 0, 1, 0)
     if direction == 3 and cuff == -1:
         tile.image = (9, 3)
+        tile.rotation = (-1, 0, 0, 0)
     return tile
+
 
 def Pit() -> Tile:
     tile = Tile()
     tile.image = (0, 5)
     return tile
 
+
 def Cog(rotation: int) -> Tile:
     tile = Tile()
+    tile.rotation = (rotation, rotation, rotation, rotation)
     if rotation == 1:
         tile.image = (6, 5)
     if rotation == -1:
         tile.image = (6, 4)
     return tile
+
 
 class Board(object):
     def __init__(self):
@@ -148,7 +204,7 @@ class Board(object):
         self.tiles[5][0] = Belt(3, 0)
         self.tiles[5][1] = Belt(3, 0)
         self.tiles[6][0] = Belt(1, 0)
-        self.tiles[6][1] = Belt(1, 1)
+        self.tiles[6][1] = Belt(2, 1)
         self.tiles[7][1] = Belt(2, 0)
         self.tiles[8][1] = Belt(2, 0)
         self.tiles[9][1] = Belt(2, 0)
@@ -166,7 +222,7 @@ class Board(object):
         self.tiles[0][3] = FastBelt(0, 0)
         self.tiles[0][5] = FastBelt(2, 0)
         self.tiles[1][5] = FastBelt(2, 0)
-        self.tiles[2][5] = FastBelt(2, 1)
+        self.tiles[2][5] = FastBelt(3, 1)
         self.tiles[2][4] = FastBelt(3, 0)
         self.tiles[4][5] = Belt(1, 0)
         self.tiles[0][6] = Belt(0, 0)
@@ -178,13 +234,13 @@ class Board(object):
         self.tiles[2][8] = Belt(2, 0)
         self.tiles[1][11] = Belt(3, 0)
         self.tiles[1][10] = Belt(3, 0)
-        self.tiles[1][9] = Belt(3, -1)
+        self.tiles[1][9] = Belt(2, -1)
         self.tiles[2][9] = Belt(2, 0)
         self.tiles[5][11] = Belt(3, 0)
         self.tiles[5][10] = Belt(3, 0)
         self.tiles[5][9] = Belt(3, 0)
         self.tiles[5][7] = Belt(3, 0)
-        self.tiles[5][6] = Belt(3, -1)
+        self.tiles[5][6] = Belt(2, -1)
         self.tiles[6][6] = Belt(2, 0)
         self.tiles[7][6] = Belt(2, 0)
         self.tiles[8][11] = Belt(3, 0)
@@ -206,7 +262,7 @@ class Board(object):
         self.tiles[8][5] = Cog(-1)
         self.tiles[8][8] = Cog(-1)
 
-        for i in [2,4,7,9]:
+        for i in [2, 4, 7, 9]:
             self.hwalls[0][i] = True
             self.hwalls[12][i] = True
             self.vwalls[i][0] = True
